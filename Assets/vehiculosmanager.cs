ee2e86f5325ed64afc44ed10d3722edfaca3bf24
MaiaMichanie <48521774@est.ort.edu.ar>
2024-06-03 09:04:13 -0300

    Arrays vehiculo cantidad de ruedas

---
+++ Assets/vehiculosmanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehiculosmanager : MonoBehaviour
+{
+    public Vehiculo vehiculoScipt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedasVehiculo = vehiculoScipt.cantRuedas;
+        Debug.Log("Cant ruedas: " + cantRuedasVehiculo);
+        if(cantRuedasVehiculo < 6 )
+        {
+            vehiculoScipt.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
